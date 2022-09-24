9c1cae33e7430278d33dc34cdd8aad9e233f0a9e
MKA1500 <maciejkabsch@gmail.com>
2022-09-24 15:54:07 +0200

    update

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,10 @@
-const API_KEY = '76a5c8ad2399e746a83375894d1907cb';
+const API_KEY = 'API_KEY';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather() {
+    return {
+        type: FETCH_WEATHER
+    };
+}
